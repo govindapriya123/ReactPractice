f564591944bd5ec9389cd7d54d498d0e057fc7f0
gkorni <govindapriya.korni@hexagon.com>
2025-12-23 19:06:23 +0530

    fix: added search functionality for todo

--- src/Todo/TodoApp.js
+++ src/Todo/TodoApp.js
@@ -1,11 +1,21 @@
-import React, {  useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 export const TodoApp = () => {
     const [todo, setTodo] = useState("");
     const [todos, setTodos] = useState([]);
+    const [search, setSearch] = useState("");
+    const [debouncedSearch, setDebouncedSearch] = useState("");
     /**
      * 
      * @returns 
      */
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedSearch(search);
+        }, [2000]);
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [search])
     const handleAddTodo = () => {
         if (!todo.trim()) return;
         setTodos(prev => [
@@ -32,20 +42,36 @@ export const TodoApp = () => {
     const handleToggleTodo = (id) => {
         setTodos(prev => prev.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo));
     }
+    /**
+     * filtered todos
+     */
+    const filteredTodos = useMemo(() => {
+        return todos.filter(todo => (todo.text.toLowerCase()).includes(debouncedSearch.toLowerCase()));
+    }, [debouncedSearch, todos]);
+    const listShown = debouncedSearch.trim() ? filteredTodos : todos;
     return (
         <div>
             <h1>TodoApp</h1>
-            <input
-                value={todo}
-                onChange={(e) => setTodo(e.target.value)}
-                placeholder="Enter Todo"
-            />
-            <button onClick={handleAddTodo}>Add Todo</button>
-            {todos.map(a => (
+            <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                <input
+                    style={{ margin: "10px" }}
+                    value={search}
+                    placeholder="Enter Search.."
+                    onChange={(e) => { setSearch(e.target.value) }}
+                />
+                <input
+                    style={{ margin: "10px" }}
+                    value={todo}
+                    onChange={(e) => setTodo(e.target.value)}
+                    placeholder="Enter Todo"
+                />
+                <button onClick={handleAddTodo}>Add Todo</button>
+            </div>
+            {listShown && listShown.length > 0 ? listShown.map(a => (
                 <div key={a.id} style={{ cursor: "pointer", textDecoration: a.completed ? "line-through" : "none" }} onClick={() => handleToggleTodo(a.id)}>
                     <p>{a.text}<span onClick={(e) => { e.stopPropagation(); handleDeleteTodo(a.id); }} style={{ color: "red", cursor: "pointer" }}>X</span></p>
                 </div>
-            ))}
+            )) : <div>No Results Found</div>}
         </div>
 
     )
